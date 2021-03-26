3709892e9b89388842ca328e41f1d21377ef8c3e
han <895240740@qq.com>
2021-03-26 18:16:15 +0800

    add

---
+++ md5.js
@@ -0,0 +1,38 @@
+// 幻数
+const A = 0x01234567;
+const B = 0x89abcdef;
+const C = 0xfedcba98;
+const D = 0x76543210;
+
+/**
+ * 小端字节处理幻数
+ * 小字节序 little-endian
+ * https://developer.mozilla.org/zh-CN/docs/Glossary/Endianness
+ * 幻数
+ */
+const 
+
+// 线性函数
+function F(x,y,z) {
+    return (x & y) | ((~x) & z)
+}
+
+function G(x,y,z) {
+    return (x & z) | (y & (~z))
+}
+
+function h(x,y,z) {
+    return x ^ Y ^ z
+}
+
+function I (x,y,z) {
+    return y ^ (x | (~z))
+}
+
+const padding = (str) => {
+    
+}
+
+function md5(str) {
+
+}
